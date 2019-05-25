30ec2ceee922354138f4e0ab3e7dc7bc51802fdd
jamesloon <mcjamesloon@gmail.com>
2019-05-24 18:05:54 -0700

    Version 1.0.1 Working.

--- Scripts/Install-module.sh
+++ Scripts/Install-module.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# > DIRECTIONS FOR THIS SCRIPT CAN ALSO BE FOUND under https://github.com/mcjamesloon/FVTT_Docker/blob/master/docs/Modules.md
+# > DIRECTIONS FOR THIS SCRIPT CAN ALSO BE FOUND under https://github.com/mcjamesloon/FVTT_Docker/blob/Dev-Testing/docs/Modules.md#scripted-module-addition
 #
 # > DOWNLOAD THIS SCRIPT via the container shell to the `public/modules folder` in the docker container.
 ## Directions for accessing the shell can be found at https://github.com/mcjamesloon/FVTT_Docker/blob/master/docs/Administration.md#accessing-the-shell
 #
-# > TO EXECUTE THIS SCRIPT you must first perform a sudo `chmod +x ./Install-module.sh` from the directory the script is installed
+# > TO EXECUTE THIS SCRIPT you must first perform a `chmod +x ./Install-module.sh` from the directory the script is installed
 #
 # > To use Text File Addition mode, place a text file with the RAW download URLs in it. One URL per line.
 #
@@ -17,16 +17,21 @@ BLUE='\033[0;33m'
 NC='\033[0m'
 #
 #Global Functions:
-echo "Loading Script."
+echo -e "${BLUE}--Loading Script--${NC}"
 #
 #Description and Directions:
 function WelcomeMessage ()
 {
-  echo -e "${RED}This script is to assist in deployment of modules in to the Foundry Virtual Tabletop Application.
-   This script has no warranties implied or otherwise. Use at your own risk.
+  echo ""
+  echo "---Jamesloon Foundry Module Addition script for Docker---"
+  echo "                  --Version 1.0.1--                      "
+  echo ""
+  echo -e "This script is to assist in deployment of modules in to the Foundry Virtual Tabletop Application.
+   ${RED}This script has no warranties implied or otherwise. Use at your own risk.
    Press CTL-C to cancel script.${NC}"
-  echo "Please ensure the general installation guide located at https://github.com/mcjamesloon/FVTT_Docker/blob/master/Scripts/README.md is followed"
-  echo "  "
+  echo ""
+  echo "Please follow the directions at https://github.com/mcjamesloon/FVTT_Docker/blob/master/docs/Modules.md#scripted-module-addition"
+  echo ""
   echo "Script Specific instructions:"
   echo "1. Access the Docker container shell."
   echo "   - Directions for this can be found at https://github.com/mcjamesloon/FVTT_Docker/blob/master/docs/Administration.md#accessing-the-shell"
@@ -34,24 +39,26 @@ function WelcomeMessage ()
   echo "2. Download or move this script into the 'public/modules' directory of the server"
   echo "   - Use 'wget <path to RAW download>' OR cp ./Install-module.sh /home/node/FVTT/public/modules/Install-module.sh"
   echo "3. Once this is done, run this script with 'bash Install-module.sh run'"
-  echo "To use Text File Addition mode, create a file named 'urls.txt' with the RAW download URLs in the 'public/modules' folder with the script. One URL per line."
-  sleep 5; exit;
+  echo ""
+  echo "NOTE: To use Text File Addition mode, create a file named 'urls.txt' with the RAW download URLs in the 'public/modules' folder with the script. One URL per line."
+  sleep 1; exit;
 }
 #
 #Begin Unzip Function
 function fUnzip ()
 {
-  echo "Listing directory"
-  ls -la
   echo "Extracting all .zip files"
-  unzip -o ./*.zip
-  echo "Listing directory"
-  ls -la
-  echo "Do you wish to clean up all .zip files from directory? If they are not removed, it will cause issues when this script is run again."
+  unzip -o './*.zip'
+  echo ""
+  echo -e "${BLUE}Listing directory:${NC}"
+  echo ""
+  ls -l
+  echo -e "${BLUE}Do you wish to clean up all .zip files from directory?${NC}
+    If they are not removed, it will cause issues when this script is run again."
   select yn in "Yes" "No"; do
     case $yn in
-      Yes ) echo "Removing files." && rm ./*.zip
-      No ) echo "Moving on. Please remove these files before running this script again."
+      Yes) echo -e "${RED}Removing files." && rm ./*.zip ; break;;
+      No) echo "Moving on. Please remove these files before running this script again." ; break;;
     esac
   done
 }
@@ -62,9 +69,9 @@ function singleAddition ()
   echo "Entered single addition mode"
   echo -e "${BLUE}Please paste in the URL of the RAW .zip module below:${NC}"
   read sURL
-  echo -e "${RED}You have entered: $sURL.${NC}"
+  echo -e "${RED}You have entered:${NC} $sURL"
   sleep 2
-  if [ $sURL == *"raw"* ]
+  if [[ $sURL == *"raw"* ]]
     then
       echo "Downloading .zip file" && wget $sURL
       fUnzip
@@ -80,8 +87,8 @@ function multiAddition ()
 {
   echo "Entered multi module addition mode."
   echo "Checking for text file."
-  lZip=ls
-  if [ $lZip == *"urls.txt"*]
+  lZip="$(ls -l)"
+  if [[ $lZip == *"urls.txt"* ]]
     then
       echo -e "Located text file. ${RED}Please note, this script will NOT validate these URLs for you. If any errors occur, please install manually or use the individual script.${NC}"
       echo "Downloading files"
@@ -90,11 +97,19 @@ function multiAddition ()
       fUnzip
       echo "Multi-module installation compleate. Please restart the server and activate the module from within Foundryvtt."
       exit
+    else
+      echo -e "${RED}NO TEXT FILE FOUND. Please add a text file named 'urls.txt' to the current directory.${NC}"
+      echo -e "${BLUE}Please choose a mode from the list below:${NC}"
+      echo "[1] Single addition mode:"
+      echo "[2] Text file addition mode:"
+  fi
 }
 # Begin Selection function
 function selection ()
 {
-  echo -e "${BLUE}Please chose a mode from the list below:${NC}"
+  echo ""
+  echo ""
+  echo -e "${BLUE}Please choose a mode from the list below:${NC}"
   echo "[1] Single addition mode:"
   echo "[2] Text file addition mode:"
   PS3='Please enter your choice: '
@@ -120,19 +135,26 @@ function selection ()
 function run ()
 {
   echo -e "Welcome to Jamesloon's module installer script for Foundry Virtual Tabletop.
-    ${RED}Please note that this script has no warranties implied or otherwise and is provided on 'as is' basis. Use at your own risk.
-    You may press CTRL-C to cancel script.${NC}"; sleep 2;
-  echo "#";
-  echo "#";
+    ${RED}Please note that this script has no warranties implied or otherwise and is provided on 'as is' basis.
+    Use at your own risk.
+    You may press CTRL-C to cancel script.${NC}"; echo "";
+  echo -e "${BLUE}Continuing in${NC}"
+  sleep 1 && echo "3";
+  sleep 1 && echo "2";
+  sleep 1 && echo "1";
   #Validate working directory to prevent accidents.
   echo "Checking directory.";
-  wrkdir=pwd
-  if [[ $wrkdir == *"public/modules" ]]; then
+  wrkdir="$(pwd)"
+  if [[ $wrkdir == *"public/modules"* ]]; then
     echo "Directory Verified. Moving on." && selection;
     else
       echo "Directory Could not be validated for this script.
-        This could be in error; however there is no harm in double checking that the script is being executed from the 'public/modules' directory."
-      echo "If you have done this and beleive this is as error, proceed with extreem caution. If you are certain, run the script with the command 'bash Install-module.sh selection'"
+        This could be in error;
+        however there is no harm in double checking that the script is being executed from:
+        'public/modules'"
+      echo -e "Directory Script found = $wrkdir
+        If you have done this and beleive this is as error;
+        run the script with the command 'bash Install-module.sh selection'"
       sleep 5
       exit;
   fi
@@ -147,8 +169,6 @@ then
 else
   # Show a helpful error
   echo "'$1' is not a known function name" >&2
-  exit 1
+  WelcomeMessage
 fi
 "$@"
-
-WelcomeMessage
